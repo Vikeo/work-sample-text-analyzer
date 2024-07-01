742a373aa7b0d9f9d6ab1307089b8c67ffc1ada6
Vikeo <thevikeo@gmail.com>
2024-07-01 22:20:10 +0200

    do it

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Button, Container, Typography } from '@mui/material';
 import './App.css';
+import { TextareaAutosize } from '@mui/base/TextareaAutosize';
+import { useRef, useState } from 'react';
+import { analyzeText } from './analysis';
 
-function App() {
+const App = () => {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const resultRef = useRef<HTMLDivElement>(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async () => {
+    if (!resultRef.current) {
+      return;
+    }
+    setLoading(true)
+    resultRef.current.innerHTML = 'Loading ...'	
+    
+    const response = await analyzeText(textAreaRef.current?.value || '');
+    const resultString = `Your text consists of ${response.numWords} words (${response.numLetters} letters)`
+
+    setLoading(false)
+    resultRef.current.innerHTML = resultString
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography color='white' fontSize={24} marginTop={4}>Text Analyzer</Typography>
+      <form onSubmit={(e) => {
+        handleSubmit();
+        e.preventDefault();
+      }}>
+          <Container sx={{display: 'flex', flexDirection: 'column', gap:2, maxWidth: "300px", mx: 'auto', marginTop:4}}>
+            <TextareaAutosize ref={textAreaRef} minRows={3} placeholder="Enter text to be counted" />
+            <Button type='submit' variant='contained' disabled={loading}>Submit</Button>
+          </Container>
+          <Typography color='white' marginTop={2} ref={resultRef}/>
+      </form>
+
     </div>
   );
 }
